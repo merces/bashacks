1919e800a37b5a266e96ffeb84fad3a64f533689
wesleyleite <wesleyhenrique@gmail.com>
2014-02-02 19:29:38 -0200

    added category hf_list_functions and iw readme

--- hack-functions.sh
+++ hack-functions.sh
@@ -36,6 +36,7 @@ hf_list_functions()
     [ "$1" == "-h" ] && { echo -e "${USAGE}" ; return 1; }
     local func
     local resume
+    local category
 
     for func in $( cat ${BASH_SOURCE} |
         grep '()$' |
@@ -43,12 +44,18 @@ hf_list_functions()
     do
         resume="$( cat ${BASH_SOURCE} |
                 sed "/^${func}/,/^}/ s/^/-/" |
-                grep '^-' | grep 'USAGE' |
-                sed 's/\\n//' |
-                head -1 |
-                cut -d\" -f2)"
-        printf '%12s\t%3s\n' "${func}" "${resume}..."
-        charcal - \* $(tput cols) 
+                grep '^-' |
+                grep -E 'USAGE|Category' |
+                cut -d\" -f2 |
+                sed 's/\\n//g;s/-/:/' |
+                tr \\n ' ' )"
+
+        category="$(echo ${resume} |
+                awk -F\: '{print $3}' |
+                sed -r 's/\$\{USAGE\}|\$1//g')"
+
+        printf '%12s\t%20s\t%3s\n' "${func}" "${category}" "$( echo ${resume} | awk -F\: '{print $1}')..."
+        charcal - \* $(tput cols)
     done
 }
 
