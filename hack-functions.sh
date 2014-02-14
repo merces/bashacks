6fe8b12c623ef5c063e3ada00c5736ecede508a9
wesleyleite <wesleyhenrique@gmail.com>
2014-02-14 09:41:51 -0200

    oui list

--- hack-functions.sh
+++ hack-functions.sh
@@ -906,11 +906,11 @@ myip()
 wscan()
 {
     local USAGE="Displays the list of wireless networks with chanell, bss, signal, ssid and others\n
-   wscan\n
+   wscan <-mac MACADDRES |-oui|-oui OUI>\n
    Category  : Networking.\n
    Parameters:
         -i  : dev wifi
-        -oui: oui 00:e0:4c return vendor, required connected
+        -oui: oui 00:e0:4c return vendor or available list 'oui'
         -mac: mac address 9c:97:26:67:f0:4b, return product company, required connected
         -h  : Help
         root is required\n
@@ -919,17 +919,23 @@ wscan()
     6	f8:1a:67:c2:be:0a	: -55.00	: Hunter
     10	9c:97:26:67:f0:4b	: -87.00	: WiFi Fon
     11	00:1a:3f:83:01:df	: -86.00	: ZUDICA\n
-   # wscan -oui 00:e0:4c 
-    ===============================================================================
-        00-E0-4C   (hex)        REALTEK SEMICONDUCTOR CORP.
-        00E04C     (base 16)    REALTEK SEMICONDUCTOR CORP.
-                                1F, NO. 11, INDUSTRY E. RD. IX
-                                SCIENCE-BASED INDUSTRIAL PARK
-                                HSINCHU 300
-                                TAIWAN, PROVINCE OF CHINA
    # wscan -mac f8:1a:67:c2:be:0a
-   TP-LINK TECHNOLOGIES CO., LTD.\n"
-
+   TP-LINK TECHNOLOGIES CO., LTD.
+   # wscan -oui 
+    BSS 00:1e:58:c4:e9:63 (on wlan0)
+	    SSID: Barack
+	    Vendor specific: OUI 00:03:7f, data: 01 01 00 20 ff 7f
+		    * Model: DIR-300
+    BSS 14:d6:4d:72:66:d4 (on wlan0)
+	    SSID: 
+	    Vendor specific: OUI 00:0c:43, data: 00 00 00 00
+   # wscan -oui 00:03:7f
+    ===============================================================================
+    00-03-7F   (hex)		Atheros Communications, Inc.
+    00037F     (base 16)	Atheros Communications, Inc.
+                                5480 Great America Parkway
+                                Santa Clara CA 95054
+                                UNITED STATES\n"
     local iFace
     local OUI
     local MAC
