90b3e858760bab55663e435ebaa81252c1a2de26
wesleyleite <wesleyhenrique@gmail.com>
2014-02-19 22:51:02 -0300

    added help sc2asm

--- hack-functions.sh
+++ hack-functions.sh
@@ -1268,8 +1268,28 @@ asminfo()
 
 sc2asm()
 {
-	local mode=32
+    local USAGE="Asm code generated through a shellcode.\n
+   sc2asm -m 32 shellcode\n
+   Category  : Reverse Engineering.\n
+   Parameters:
+        -m     : followed by architecture and string shellcode
+        -h     : Help.
+   Output:
+   \$ sc2asm '\\\x31\\\xc0\\\x40\\\x40\\\xcd\\\x80\\\xeb\\\xf8'
+   xor eax, eax
+   inc eax
+   inc eax
+   int 0x80
+   jmp 0x0\n"
+
+    local mode=32
 	local in="$1"
+
+    [ $# -eq 0 -o \
+        "$1" = '-h' ] && {
+        echo -e "${USAGE}"
+        return 1
+    }
 
 	[ $1 = '-m' ] && mode=$2 in="$3"
 
