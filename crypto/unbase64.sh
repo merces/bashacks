c73faa4cd6ac553fdfe9dc27426d153d82da18cd
wesleyleite <wesleyhenrique@gmail.com>
2014-04-30 22:57:29 -0300

    unbase64

---
+++ crypto/unbase64.sh
@@ -0,0 +1,20 @@
+unbase64()
+{
+   local USAGE="Decodes a BASE64 string.\n
+   unbase64 <string base64>\n
+   Category  : Crypto.\n
+   Parameters:
+        -h     : Help.
+        string : Base64 encoded string.\n
+   Output:
+   \$ unbase64 b3p6eSBvc2JvdXJuZQ==
+   ozzy osbourne"
+
+    [ $# -eq 0 -o "$1" == '-h' ] && { 
+        echo -e "${USAGE}"
+        return 1
+    }
+    
+    echo $1 | base64 -d
+    echo
+}
