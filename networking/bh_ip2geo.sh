336daf4851b441be3c1c8053707e532525127267
wesleyleite <wesleyhenrique@gmail.com>
2014-12-02 00:44:21 -0200

    prefixed bh_ip2geo

--- networking/bh_ip2geo.sh
+++ networking/bh_ip2geo.sh
@@ -1,13 +1,13 @@
-ip2geo()
+bh_ip2geo()
 {
     local USAGE="Approximately determines geographical location of an ip address or domain name.\n
-   ip2geo <ip address>\n
+   bh_ip2geo <ip address>\n
    Category  : Networking.\n
    Parameters:
         -h        : Help.
         Ip adrress: Ip to determine location.\n
    Output:
-   \$ ip2geo www.mentebinaria.com
+   \$ bh_ip2geo www.mentebinaria.com
    www.mentebinaria.com.br New Dream Network, LLC New Dream Network, LLC Brea US 33.926898956299 -117.86119842529 1"
 
     [ $# -eq 0 -o "$1" == '-h' ] && {
