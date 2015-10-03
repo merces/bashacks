36d6a1a67a52f9503a4519750e43ad078ffc7c8d
wesleyleite <wesleyhenrique@gmail.com>
2015-10-03 01:51:35 -0300

    update bh_unmd5 and added bh_unsha1 bh_unsha256 bh_unsha356 bh_unsha512

--- crypto/bh_unmd5.sh
+++ crypto/bh_unmd5.sh
@@ -3,19 +3,19 @@ bh_unmd5()
     [ $# -eq 0 ] && return 1
 
     local sHash="$1"
-    local sSite="http://www.md5online.org/"
+    local sSite="http://hashtoolkit.com/reverse-hash/?hash=$sHash"
 
-    sA=$(wget -T 30 -q -O - "${sSite}" \
-    --user-agent='Mozilla/5.0' |
-    grep -Ewo 'hidden" name="a" value="[0-9]+' |
-    grep -Ewo '[0-9]+')
+    sA=$(wget -T 30 -q -O - "$sSite" --user-agent="Mozilla/5.0 (Windows NT 6.1; WOW64; rev:28.0) Gecko'20100101 Firefox/28.0" |
+        grep -A 1 'res-text' |
+        tail -1 |
+        sed -e 's/[ ]\+//g ;s/<[^>]*>//g')
 
-    [ ! -z ${sA} ] && { 
-        wget -q -T 30 -O - "${sSite}" \
-        --user-agent='Mozilla/5.0' \
-        --post-data="md5=${sHash}&search=0&action=decrypt&a=${sA}" |
-        grep -Ewo 'Found : <b>.+</b>' |
-        cut -d ':' -f2 |
-        sed -r 's/<b>|<\/b>|^ //g'
-    } || echo "variable 'a' not found"
+    [ ! -z "$sA" ] && [ "$sA" != "$sHash" ] &&
+        echo "${sA}"
 }
+
+alias bh_unsha512="bh_unmd5"
+alias bh_unsha1="bh_unmd5"
+alias bh_unsha256="bh_unmd5"
+alias bh_unsha356="bh_unmd5"
+
