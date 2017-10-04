9905e34fd77df4968ae730b34f237e671227761b
Wesley Leite <wesleyhenrique@gmail.com>
2017-10-04 20:14:22 -0300

    remove progress from bh_wget not supported in the new version wget

--- src/networking/bh_wget.sh
+++ src/networking/bh_wget.sh
@@ -2,5 +2,5 @@ bh_wget()
 {
 	[ "$1" ] || return
 	local ua='Mozilla/5.0 (Windows NT 6.1; WOW64; rv:28.0) Gecko/20100101 Firefox/28.0'
-	wget -q --show-progress -c --user-agent "$ua" "$1"
+	wget -c --user-agent "$ua" "$1"
 }
