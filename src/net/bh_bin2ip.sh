a4e2514a33add7b048b6d86795738f8686b03961
Fernando Mercês <fernandom@zemaria.local>
2018-02-18 02:31:37 -0300

    macOS fixes

--- src/net/bh_bin2ip.sh
+++ src/net/bh_bin2ip.sh
@@ -1,12 +1,7 @@
 bh_bin2ip()
 {
-    local sBin="$(echo $1 |
-                grep -Ewo '^(([0-1]){8}\.){3}([0-1]){8}$')"
-
-    [ $# -eq 0 -o -z "${sBin}" ] && return 1 
-
-	local i
-	for i in $(echo "${sBin}" | tr . ' '); do
-	    printf "%d." $(bh_bin2dec $i)
-    done | sed "s/.$/\\n/"
+	local a=b=c=d=
+	
+	IFS=. read a b c d <<< "$1"								
+	echo $((2#$a)).$((2#$b)).$((2#$c)).$((2#$d))
 }
