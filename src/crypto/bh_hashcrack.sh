7302cd2d211c62c32c01e9249d677ece1d5cfae3
André Traleski de Campos <andretraleskic@hotmail.com>
2018-08-12 00:55:38 -0300

    Minor bug fix on hashcrack function

--- src/crypto/bh_hashcrack.sh
+++ src/crypto/bh_hashcrack.sh
@@ -2,7 +2,7 @@ bh_hashcrack() {
     [[ $# -eq 0 ]] && return 1
 
     local hash="$1"
-    local site="http://hashtoolkit.com/reverse-hash/?hash=$sHash"
+    local site="http://hashtoolkit.com/reverse-hash/?hash=$hash"
 
     res=$(bh_cmd_wget -qO - "$site" \
      | grep -FA1 'res-text' \
