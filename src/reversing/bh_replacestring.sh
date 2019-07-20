3d2a9d49aa1f12f698824ccaaceff4ce8cb14444
Jeremias Moreira Gomes <j3r3miasmg@gmail.com>
2019-07-20 17:19:17 -0300

    bh_replacestring(): Replace strings that has the same size.

---
+++ src/reversing/bh_replacestring.sh
@@ -0,0 +1,15 @@
+bh_replacestring() {
+	[[ -f "$1" && -n "$2" && -n "$3" && "${#2}" == "${#3}" ]] || return 1
+
+	local fil="$1"
+	local src="$2"
+	local dst="$3"
+
+    local srchex=$(echo $src | xxd -pu)
+    local dsthex=$(echo $dst | xxd -pu)
+
+	local tmpfile=".$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)"
+
+    xxd -p < $fil | tr -d '\n' | sed "s/${srchex::-2}/${dsthex::-2}/g" | xxd -r -p > $tmpfile
+	cat $tmpfile > $fil && rm $tmpfile
+}
