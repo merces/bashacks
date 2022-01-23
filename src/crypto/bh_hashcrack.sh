b54342e20b11d9475fd222f8532c158016e933dd
0x6775737461 <fabiogustavo@tuta.io>
2022-01-23 08:25:39 -0400

    made the coding working again

--- src/crypto/bh_hashcrack.sh
+++ src/crypto/bh_hashcrack.sh
@@ -1,12 +1,11 @@
 bh_hashcrack() {
-    [[ $# -eq 0 ]] && return 1
+    [ $# -eq 0 ] && return 1
 
     local hash="$1"
-    local site="http://hashtoolkit.com/reverse-hash/?hash=$hash"
+    local site="https://hashtoolkit.com/decrypt-hash/?hash=$hash"
 
     res=$(bh_cmd_wget -qO - "$site" \
-     | grep -FA1 'res-text' \
-     | sed -n 's/^.*<span>\(.*\)<\/span>.*$/\1/p')
+	  | sed -n '/.*generate-hash\/?text=\(.*\)\".*/{s//\1/p;q;}')
 
-    [[ "$res" != "$hash" ]] && echo "$res" | sort -u
+    [ "$res" != "$hash" ] && echo "$res"
 }
