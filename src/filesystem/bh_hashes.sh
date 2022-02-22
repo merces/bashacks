a8d21cda03d2ab97d39bdf3d344a9bb9dc3eeada
wesleyleite <1591080+wesleyleite@users.noreply.github.com>
2022-02-21 23:06:03 -0300

    Issue #46 update filesystem

--- src/filesystem/bh_hashes.sh
+++ src/filesystem/bh_hashes.sh
@@ -2,11 +2,10 @@ bh_hashes() {
 	(( $# < 1 )) && return 1
 
 	local i
-	
+
 	for i in $*; do
-		bh_cmd_md5 "$i"
-		bh_cmd_sha1 "$i"
-		bh_cmd_sha256 "$i"
-		#bh_cmd_sha512 "$i"
+		echo $(bh_cmd_md5 "$i")
+		echo $(bh_cmd_sha1 "$i")
+		echo $(bh_cmd_sha256 "$i")
 	done | tr -s ' ' ' '
 }
