003d6cc7e71e1bdef01a08647971b9c3de290b2a
merces <nandu88@gmail.com>
2024-11-27 19:37:27 -0300

    Explit HTTP scheme for wget

--- src/reversing/bh_asminfo.sh
+++ src/reversing/bh_asminfo.sh
@@ -3,16 +3,16 @@ bh_asminfo() {
 
     local ins=$1
 
-    [[ -d $BASHACKS_CACHEDIR ]] || mkdir -p $BASHACKS_CACHEDIR
+    [[ -d $bashacks_cachedir ]] || mkdir -p $bashacks_cachedir
 
-    if [[ -s $BASHACKS_CACHEDIR/$ins.txt ]]; then
-        cat $BASHACKS_CACHEDIR/$ins.txt
+    if [[ -s $bashacks_cachedir/$ins.txt ]]; then
+        cat $bashacks_cachedir/$ins.txt
     else
-        bh_cmd_wget -q faydoc.tripod.com/cpu/$ins.htm -O - \
+        wget -T10 -U "${bashacks_wget_user_agent}" \
+         -q https://faydoc.tripod.com/cpu/$ins.htm -O - \
          | html2text \
          | sed -n '/^===.*/,$p' \
-         | sed 's/^===.*/'${ins}'/' \
-         | tr _ ' ' \
-         | tee -a $BASHACKS_CACHEDIR/$ins.txt
+         | sed 's/^===.*/'${ins}'/;s/_/ /g' \
+         | tee -a $bashacks_cachedir/$ins.txt
     fi
 }
