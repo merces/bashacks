ae1969508adacc612342679400b58b9275eebc27
merces <nandu88@gmail.com>
2024-11-27 19:40:29 -0300

    Pure wget as User-Agent was causing problems

--- src/net/bh_unshort.sh
+++ src/net/bh_unshort.sh
@@ -1,5 +1,5 @@
 bh_unshort() {
     (( $# < 1 )) && return 1
     
-    bh_cmd_wget --max-redirect 0 --spider "$1" 2>&1 | grep -E '^Loca(tion|liza..o):' | cut -d' ' -f2
+    wget -T10 --max-redirect 0 --spider "$1" 2>&1 | grep -E '^Loca(tion|liza..o):' | cut -d' ' -f2
 }
